4246ad197d0b97e7289b51a41a7bdd748069b7bf
kirillVladov <vznaniya@bk.ru>
2023-09-19 02:56:07 +0500

    CT-T-10 connect actions page and add filter

--- src/services/api/requests/actions.ts
+++ src/services/api/requests/actions.ts
@@ -1,15 +1,17 @@
 import $http from "../http";
 import { Action } from "../../../../types/tasks/Actions";
+import { ActionTab } from "../../../../types/actions";
+import { SuccessResponse } from "../../../../types/common/responses";
 
 const base = "/actions";
-export async function updateActionsList() {
-  return await $http.get(`${base}/list`);
+export async function updateActionsList(status?: ActionTab) {
+  return await $http.get(`${base}/list`, { params: { status } });
 }
 
 export async function addAction(data: Action) {
   return await $http.post(base, data);
 }
 
-export async function deleteAction(actionId: string) {
+export async function deleteAction(actionId: string): Promise<SuccessResponse> {
   return await $http.delete(base, { params: { id: actionId } });
 }
