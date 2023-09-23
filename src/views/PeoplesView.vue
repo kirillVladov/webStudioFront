1086d4c7bdf0a822988c864f3b0d5aee10983d27
kirillVladov <vznaniya@bk.ru>
2023-09-23 18:52:02 +0500

    CT-T-7 peoples page

--- src/views/PeoplesView.vue
+++ src/views/PeoplesView.vue
@@ -2,21 +2,35 @@
 import { onBeforeMount } from "vue";
 import { useUserStroe } from "../stores/user";
 import PeopleItem from "../components/peoples/PeopleItem.vue";
+import FilterComponent from "../components/common/FilterComponent.vue";
 
 const userStore = useUserStroe();
 
 onBeforeMount(() => {
   userStore.updateAllUsers();
 });
+
+const TEMP_PEOPLES = [
+  {
+    name: "employers",
+    value: "employers",
+  },
+  { name: "workers", value: "workers" },
+];
 </script>
 
 <template>
   <div class="peoples">
-    <people-item
-      v-for="(user, idx) in userStore.getAllUsers"
-      :key="`people-item-${idx}-${user.name}`"
-      :user="user"
-    />
+    <div class="peoples__header">
+      <filter-component :items="TEMP_PEOPLES" />
+    </div>
+    <div class="peoples__list">
+      <people-item
+        v-for="(user, idx) in userStore.getAllUsers"
+        :key="`people-item-${idx}-${user.name}`"
+        :user="user"
+      />
+    </div>
   </div>
 </template>
 
